fa02d6388a411030fcca49d61b5e53706f3a72b7
Do Huynh Nhut <huynhnhutdo@gmail.com>
2021-01-10 18:25:35 +0700

    #35 code layout side-bar customer

---
+++ resources/views/common/customer/left-sidebar.blade.php
@@ -0,0 +1,13 @@
+<ul data-view-id="main_navigation" style="z-index:2" class="jJSxyD">
+    <li data-view-id="main_navigation_item" class=" menu-header-left"><a class="menu-link" rel="" href=""><span
+                class="icon-wrap"><i class="icon-main-menu-tv lv1-icon tikicon"></i></span><span class="text">Thông tin tài khoản</a>
+    </li>
+    <li data-view-id="main_navigation_item" class=" menu-header-left"><a class="menu-link" rel="" href=""><span
+                class="icon-wrap"><i class="icon-main-menu-tv lv1-icon tikicon"></i></span><span class="text">Quản lý đơn hàng</span>
+        </a>
+    </li>
+    <li data-view-id="main_navigation_item" class=" menu-header-left"><a class="menu-link" rel="" href=""><span
+                class="icon-wrap"><i class="icon-main-menu-tv lv1-icon tikicon"></i></span><span class="text">Sản phẩm yêu thích</span>
+        </a>
+    </li>
+</ul>
